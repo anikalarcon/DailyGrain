1c1822c1b39d723ffe4689bd8f34787825161c09
anikalarcon <80154816+anikalarcon@users.noreply.github.com>
2021-03-06 20:27:22 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html style="background: rgb(57,62,70);">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min1.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Team-Grid.css">
+	<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+</head>
+
+<body style="background: #FFFFFF">
+    <header>
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav" style="background: #F5EEE6;">   
+		<div class="container"><a class="navbar-brand js-scroll-trigger" href="index.html" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">DailyGrain</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler float-right" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">Home</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">Products</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">Take Quiz</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">Sack</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">Order Summary</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="" style="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav><<br><br>
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<div class="team-grid">
+        <div class="container">
+            <div class="row people">
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-color: #000000;border-radius: 17px;">
+                        <div class="cover" style="background: RGB(54,79,107,0.62);">
+                            <h3 class="name">HAKDOG</h3>
+                            <p class="title"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-color: #000000;border-radius: 17px;">
+                        <div class="cover" style="background: rgba(67,129,132,0.79);">
+                            <h3 class="name">HAKDOG</h3>
+                            <p class="title"></p>  
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-color: #000000;border-radius: 17px;">
+                        <div class="cover" style="background: RGB(95,90,114,0.68);">
+                            <h3 class="name">HAKDOG</h3>
+                            <p class="title"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3 item">
+                    <div class="box" style="background-color: #000000;border-radius: 17px;">
+                        <div class="cover" style="background: rgba(54,79,107,0.81);">
+                            <h3  class="name">HAKDOG</h3>
+                            <p  class="title"></p>
+                        </div>
+                    </div>
+                </div>
+				<div class="masthead-content" style="padding-top: 192px;padding-bottom: 192px;">
+						<h1 class="font-family: Montserrat, sans-serif;font-weight: bold;color: #36213E;letter-spacing: 11px;">"All time favorites"</h1>
+						<p><button class="btn btn-primary" type="button" style="onclick=">See more products</button></p>
+				</div>
+            </div>
+        </div>
+    </div>
+        
+    </header>
+    <footer class="py-5" style="background: #36213E;">
+        <div class="container">
+            <p class="text-center text-white m-0 small">Copyright&nbsp;© Brand 2021</p>
+        </div>
+    </footer>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.min1.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/script.min1.js"></script>
+</body>
+
+</html>
